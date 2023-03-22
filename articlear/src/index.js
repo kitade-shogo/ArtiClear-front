8d0ce25714ee5711e63165b9f9f818a267c8ed36
kitadeshogo <tori3227@gmail.com>
2023-03-22 21:58:57 +0900

    NextUIの適用

--- articlear/src/index.js
+++ articlear/src/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+
+const theme = createTheme({
+  type: "dark", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: "$green200",
+      primaryLightHover: "$green300",
+      primaryLightActive: "$green400",
+      primaryLightContrast: "$green600",
+      primary: "#4ADE7B",
+      primaryBorder: "$green500",
+      primaryBorderHover: "$green600",
+      primarySolidHover: "$green700",
+      primarySolidContrast: "$white",
+      primaryShadow: "$green500",
+
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
+      link: "#5E1DAD",
+
+      // you can also create your own color
+      myColor: "#ff4ecd",
+
+      // ...  more colors
+      background: '',
+    },
+    space: {},
+    fonts: {},
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider theme={theme}>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
